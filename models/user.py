2b5c343fd2a4e9b7066e36a791369dcf24075ebe
marcosrodg <marcosrodg26@gmail.com>
2022-01-02 02:09:27 -0300

    Criação de funções de busca

--- models/user.py
+++ models/user.py
@@ -4,11 +4,12 @@ import hashlib
 class UserModel(db.Model):
     __tablename__ = 'users'
     
-    user_id = db.Column(db.Integer, primary_key=True)
+    user_id_cpf = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50))
     password = db.Column(db.String(128))
     
-    def __init__(self,email,password):
+    def __init__(self,cpf,email,password):
+        self.cpf = cpf
         self.email = email
         self.password = password
         
@@ -21,7 +22,18 @@ class UserModel(db.Model):
         m.update(word.encode('utf-8'))
         return m.hexdigest()
     
-
+    @classmethod
+    def find_by_email(cls,email):
+        user = cls.query.find_by(email=email).first()
+        return user
+    
+    @classmethod
+    def find_by_cpf(cls,cpf):
+        user = cls.query.find_by(cpf=cpf)
+        return user
+    
+    
+    
            
         
                       
