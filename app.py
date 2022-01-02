3c203fe30dd906dd37df1a3004a10e25fe27250c
marcosrodg <marcosrodg26@gmail.com>
2022-01-02 02:54:05 -0300

    Correção funções de buscapython

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
 from flask_restful import Api
-from resources.user import UserLogin
+#from resources.user import UserLogin
+from resources.user import RegisterUsers
+from sql_alchemy import db
 
 
 
 app = Flask(__name__)
-api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-api.add_resource(UserLogin,'/login')
+
+api = Api(app)
+db.init_app(app)
+
+@app.before_first_request
+def create_db():
+    db.create_all()
+
+
+
+#api.add_resource(UserLogin,'/login')
+api.add_resource(RegisterUsers,'/login/register')
+
+if __name__ == '__main__':
+    app.run(debug=True)
