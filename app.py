940886855083ecad6269fdee068ebf75aad76395
marcosrodg <marcosrodg26@gmail.com>
2022-01-02 05:08:45 -0300

    Configuração de variaveis

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_restful import Api
 #from resources.user import UserLogin
-from resources.user import RegisterUsers
+from resources.user import RegisterUsers, Login
 from sql_alchemy import db
+from flask_jwt_extended import JWTManager
+
 
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+
 
 
 api = Api(app)
 db.init_app(app)
+jwt = JWTManager(app)
+
 
 @app.before_first_request
 def create_db():
@@ -20,7 +26,7 @@ def create_db():
 
 
 
-#api.add_resource(UserLogin,'/login')
+api.add_resource(Login,'/login')
 api.add_resource(RegisterUsers,'/login/register')
 
 if __name__ == '__main__':
