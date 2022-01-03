0e3b900960626dd01bf6ca152861db8e169040da
marcosrodg <marcosrodg26@gmail.com>
2022-01-03 19:34:38 -0300

    Adicionando variaveis jwt

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ from flask_jwt_extended import JWTManager
 
 
 
-
 app = Flask(__name__)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+app.config['JWT_SECRET_KEY'] = 'secret' # Mude isso
+app.config['JWT_ALGORITHM'] = 'HS512'
 
 
 
